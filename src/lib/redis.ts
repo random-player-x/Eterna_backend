84c435a6cdb701c6432ad7afc318db125de80b72
Utkarsh <utkarsh@Utkarshs-MacBook-Air-2811.local>
2025-11-09 23:00:53 +0530

    updated the redis connection - switched to deployed one

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,3 +1,3 @@
 import Redis from "ioredis";
 
-export const redis = new Redis({ host: "127.0.0.1", port: 6379 });
+export const redis = new Redis(process.env.UPSTASH_REDIS_REST_URL || "redis://127.0.0.1:6379")
--- src/scheduler/queue.ts
+++ src/scheduler/queue.ts
@@ -1,6 +1,5 @@
 
 import Queue from "bull";
 
-export const priceUpdateQueue = new Queue("price-updates", {
-  redis: { host: "127.0.0.1", port: 6379 }
-});
+
+export const priceUpdateQueue = new Queue("price-updates", process.env.UPSTASH_REDIS_REST_URL || "redis://127.0.0.1:6379");
